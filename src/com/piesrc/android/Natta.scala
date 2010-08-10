baf6078e05557372e283bae7173051869dce7d4a
Andy Driver <andy@pagezero.net>
2010-08-10 22:50:33 +0100

    Added a ListView for tweets

--- src/com/piesrc/android/Natta.scala
+++ src/com/piesrc/android/Natta.scala
@@ -1,16 +1,24 @@
 package com.piesrc.android
 
-
-// we need these _root_ because com.android exists,
-// and we are in com.example.helloscala.
 import _root_.android.app.Activity
+import _root_.android.content.Context
 import _root_.android.os.Bundle
+import _root_.android.widget.{ArrayAdapter, ListView}
 
-
-// Not another terrible twitter app
 class Natta extends Activity {
-  override def onCreate(savedInstanceState: Bundle) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main)
-  }
+
+    private var tweetList: ListView = _
+
+    override def onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main)
+
+        tweetList = findViewById(_root_.android.R.id.list).asInstanceOf[ListView]
+        val tweets = Array(
+            "Hello", "Twitter", "World"
+        )
+        val adapter = new ArrayAdapter[String](this, R.layout.list_item, tweets)
+        tweetList setAdapter adapter
+    }
 }
