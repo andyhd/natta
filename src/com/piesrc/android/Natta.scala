d5985fabf6e767dd6bcb4cb22354b95bc3f6f493
Andy Driver <andy@pagezero.net>
2010-08-11 13:27:41 +0100

    Added text field and send button

--- src/com/piesrc/android/Natta.scala
+++ src/com/piesrc/android/Natta.scala
@@ -3,21 +3,27 @@ package com.piesrc.android
 import _root_.android.app.Activity
 import _root_.android.content.Context
 import _root_.android.os.Bundle
-import _root_.android.widget.{ArrayAdapter, ListView}
+import _root_.android.widget.{ArrayAdapter, Button, EditText, ListView}
 
 class Natta extends Activity {
 
+    private var newTweet: EditText = _
     private var tweetList: ListView = _
+    private var sendBtn: Button = _
 
     override def onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.main)
 
-        tweetList = findViewById(_root_.android.R.id.list).asInstanceOf[ListView]
+        tweetList = findViewById(R.id.list).asInstanceOf[ListView]
+        newTweet = findViewById(R.id.textField).asInstanceOf[EditText]
+        sendBtn = findViewById(R.id.button).asInstanceOf[Button]
+
         val tweets = Array(
             "Hello", "Twitter", "World"
         )
+
         val adapter = new ArrayAdapter[String](this, R.layout.list_item, tweets)
         tweetList setAdapter adapter
     }
